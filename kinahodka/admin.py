fce6fcc56d48606ffea82b817dbe0196e6f5dafb
Madridka <serbin_kirill1807@mail.ru>
2023-09-25 04:01:09 +0700

    Initial

---
+++ kinahodka/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Author, Film, Genre, Language, Country
+
+admin.site.register(Author)
+admin.site.register(Film)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Country)
---
+++ kinahodka/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=25,
+                                  verbose_name='Имя режиссера')
+    last_name = models.CharField(max_length=25,
+                                 verbose_name='Фамилия режиссера')
+
+    def __str__(self):
+        return self.last_name
+
+
+class Film(models.Model):
+    title = models.CharField(max_length=100,
+                             verbose_name='Название книги')
+    year_out = models.CharField(max_length=10,
+                                verbose_name='Год выпуска')
+    description = models.CharField(max_length=1000,
+                                   verbose_name='Сюжет фильма')
+
+    author = models.ManyToManyField('Author',
+                                    verbose_name='Режиссер')
+    language = models.ManyToManyField('Language',
+                                      verbose_name='Язык')
+
+    country = models.ForeignKey('Country', on_delete=models.CASCADE,
+                                verbose_name='Страна')
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE,
+                              verbose_name='Жанр')
+
+    url_kp = models.URLField(verbose_name='Ссылка на KINOPOISK')
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    genre = models.CharField(max_length=100,
+                             verbose_name='Жанр')
+
+    def __str__(self):
+        return self.genre
+
+
+class Language(models.Model):
+    language = models.CharField(max_length=20,
+                                verbose_name='Язык')
+
+    def __str__(self):
+        return self.language
+
+
+class Country(models.Model):
+    country = models.CharField(max_length=20,
+                               verbose_name='Страна')
+
+    def __str__(self):
+        return self.country
